e286bf3fd8e48f7958a1d1f673bbf3f7b6eefddc
A721\1dam <javcarcor2@alu.edu.gva.es>
2024-11-28 16:18:35 +0100

    1 e

--- EXAMEN UD3/src/Examen.java
+++ EXAMEN UD3/src/Examen.java
@@ -1,5 +1,54 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Examen {
     public void ejercicio1(){
+        Scanner entrada=new Scanner(System.in);
+        Random random=new Random();
+        System.out.println("*** BIENVENIDO AL BINGO DEL CASINO CANTÁBRICO ***");
+
+        int num_bolas= random.nextInt(10,40);
+        int resultado[]=new int[num_bolas];
+
+        for (int i = 0; i < num_bolas; i++) {
+            resultado[i]=random.nextInt(1,90);
+        }
+        Arrays.stream(resultado).distinct();
+
+        System.out.println(num_bolas+ " bolas extraídas hasta ahora: "+ Arrays.toString(resultado));
+
+        System.out.println("*** Introduce los datos de tu cartón ***");
+        int carton[][]=new int[3][3];
+        boolean comp=false;
+        for (int i = 0; i < carton.length; i++) {
+            System.out.println("Fila "+ (i+1));
+            String fila=entrada.next();
+
+            if (fila.matches("[0-9]+-[0-9]+-[0-9]+")){
+                String filas[]=fila.split("-");
+                for (int j = 0; j < carton[0].length; j++) {
+                    carton[i][j]=Integer.valueOf(filas[j]);
+                }
+                comp=true;
+            }else {
+                System.out.println("Cerrando programa... Introduce valores con el formato correcto (N-N-N)");
+                comp=false;
+                break;
+            }
+        }
+
+        if (comp){
+            System.out.println("Datos del cartón introducido:");
+            for (int i = 0; i < carton.length; i++) {
+                for (int j = 0; j < carton[i].length; j++) {
+                    System.out.print(carton[i][j]+" ");
+                }
+                System.out.print("\n");
+            }
+            System.out.println(" ");
+            System.out.println("\nPREMIO");
+        }
 
     }
 }
